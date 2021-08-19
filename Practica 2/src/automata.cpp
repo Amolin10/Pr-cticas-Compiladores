b95b2ae23fa1de7e6138b040deec82023a9b33fe
Amolin10 <molina102997@gmail.com>
2021-08-18 19:07:55 -0500

    Prueba

--- Practica 2/src/automata.cpp
+++ Practica 2/src/automata.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/* Uso de Type enum
 int main ( ) {
-    std::cout << "Hola";
-}
+    
+    enum tipo {entero, flotante};
+    tipo numero = flotante;
+    if (numero == entero) {
+        std::cout << "Hola";
+    } else if (numero == flotante) {
+        std::cout << "Adios";
+    }
+
+}*/
